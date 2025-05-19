4a20e015e357aa3ea8c78a00819667ba56afc87d
ARYAN-095 <aryanshukla095@gmail.com>
2025-05-19 07:02:46 +0000

    setup directory

---
+++ backend/src/index.ts
@@ -0,0 +1,26 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+ app.post('api/v1/signup',  (c)=>{
+        return c.text("Hello world")
+ });
+
+ app.post('api/v1/login', (c)=>{
+        return c.text("Hello world")
+ });
+
+ app.post('api/v1/blog', (c)=>{
+        return c.text("Hello world")
+ });
+
+ app.put('api/v1/blog', (c)=>{
+        return c.text("Hello world")
+ });
+
+ app.get('api/v1/blog',(c)=>{
+        return c.text("Hello world")
+ });
+
+export default app
+ 
